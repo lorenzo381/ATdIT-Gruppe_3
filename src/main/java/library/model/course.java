5db0c89f97c0bc3d5fc212e24e45539c95b49f10
Marco Lorenz <marco.lorenz01@sap.com>
2021-05-06 16:56:14 +0200

    UML implementation 03.05.: implementation student/teacher

---
+++ src/main/java/library/model/course.java
@@ -0,0 +1,6 @@
+package library.model;
+
+import java.util.List;
+
+public interface course extends List<person> {
+}
---
+++ src/main/java/library/model/implementation/BasicCourse.java
@@ -0,0 +1,9 @@
+package library.model.implementation;
+
+import library.model.person;
+import library.model.course;
+
+import java.util.ArrayList;
+
+public class BasicCourse extends ArrayList<person> implements course {
+}
