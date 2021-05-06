2bc20aa5f217dad153fbec2a6e2ae26666b08edf
JonasJamin <jonas.jamin@gmx.de>
2021-05-06 22:10:39 +0200

    Datenbank Anbindung Groundwork

---
+++ src/main/java/main/DBConnect.java
@@ -0,0 +1,18 @@
+
+// Schnitstelle zwischen Datenbank und rest des Programms von uns
+
+public class DBConnect {
+
+public static final String location="jdbc:sqlite:src/main/java/library/Database/db.sqlite";
+
+    public static Connection connect() {
+
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(location);
+            System.out.println("Connection to Database established");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
