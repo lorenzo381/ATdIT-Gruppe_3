f0c78aa0a9245b17069885cf21e5884eee37d935
lucaIsaak <luca.isaak@kabelbw.de>
2021-05-17 01:05:00 +0200

    StudentOverview für Datenbank vorbereitet

--- src/main/java/library/presentation/StudentOverviewScreen.java
+++ src/main/java/library/presentation/StudentOverviewScreen.java
@@ -5,12 +5,15 @@ import java.awt.*;
 public class StudentOverviewScreen extends JFrame {
 
     Container pane;
-    JPanel idJP, familynameJP, firstnameJP, missedDaysJP, qualityJP ,quantityJP ,gradeJP, homeworkJP;
     JPanel white, description;
-    JPanel one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve;
-    JPanel jpArray [] = {one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve};
-    JLabel jp, jl, lastname, firstname, coursename, frequancy, missedDays, grade;
-    String arrayDescriptions [] = {"ID","lastname", "firstname", "frequency", "course name", "missed days", "grade"};
+
+    JPanel zero,one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen;
+    JPanel jpArray [] = {zero,one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen };
+    JLabel jp , jlLastName, jlFirstName, jlCourseName,  jlMissedDays, jlGrade;
+    JLabel jlArray[] = { jlLastName, jlFirstName, jlCourseName,  jlMissedDays, jlGrade};
+
+
+    String arrayDescriptions [] = {"lastname", "firstname",  "course name", "missed days", "grade"};
 
     public StudentOverviewScreen() {
         setExtendedState(Frame.MAXIMIZED_BOTH);
@@ -19,13 +22,13 @@ public class StudentOverviewScreen extends JFrame {
         //setUndecorated(true);
 
         pane = getContentPane();
-        pane.setLayout(new GridLayout(15,1));
+        pane.setLayout(new GridLayout(20,1));
 
         white = new JPanel();
         white.setBackground(Color.white);
         pane.add(white);
 
-        description = new JPanel(new GridLayout(1,7));
+        description = new JPanel(new GridLayout(1,5));
 
         for (int i = 0; i<arrayDescriptions.length; i++) {
             jp = new JLabel(arrayDescriptions[i], JLabel.CENTER);
@@ -38,22 +41,54 @@ public class StudentOverviewScreen extends JFrame {
         }
         pane.add(description);
 
-        for (int i =0; i<jpArray.length; i++) {
+        System.out.println(jpArray.length);
+
+
+
+
+
+        for (int i =1; i<jpArray.length; i++) {
             jpArray[i] = new JPanel();
-            jpArray[i].setLayout(new GridLayout(1,7));
-            for ( int j =0; j<7; j++) {
+            jpArray[i].setLayout(new GridLayout(1,5));
 
-                 jl = new JLabel ();
-                 jl.setBackground(Color.lightGray);
-                 jl.setOpaque(true);
-                 jl.setOpaque(true);
-                 jl.setBorder(BorderFactory.createMatteBorder(0,1,2,1, Color.white));
-                 jpArray[i].add(jl);
+            jlLastName = new JLabel(/*DatabaseController.getLastName(i)*/);
+            jlFirstName = new JLabel(/*DatabaseController.getFirstName(i)*/);
+            jlCourseName = new JLabel(/*DatabaseController.getCourseName(i)*/);
+            jlMissedDays = new JLabel(/*DatabaseController.getMissedDays(i)*/);
+            jlGrade = new JLabel(/*DatabaseController.getGrade()*/);
 
 
-            }
+            jlLastName.setBackground(Color.white);
+            jlFirstName.setBackground(Color.white);
+            jlCourseName.setBackground(Color.white);
+            jlMissedDays.setBackground(Color.white);
+            jlGrade.setBackground(Color.white);
+
+            jlLastName.setOpaque(true);
+            jlFirstName.setOpaque(true);
+            jlCourseName.setOpaque(true);
+            jlMissedDays.setOpaque(true);
+            jlGrade.setOpaque(true);
+
+            jlLastName.setBorder(BorderFactory.createMatteBorder(0, 1, 2, 1, Color.lightGray));
+            jlFirstName.setBorder(BorderFactory.createMatteBorder(0, 1, 2, 1, Color.lightGray));
+            jlCourseName.setBorder(BorderFactory.createMatteBorder(0, 1, 2, 1, Color.lightGray));
+            jlMissedDays.setBorder(BorderFactory.createMatteBorder(0, 1, 2, 1, Color.lightGray));
+            jlGrade.setBorder(BorderFactory.createMatteBorder(0, 1, 2, 1, Color.lightGray));
+
+            jpArray[i].add(  jlLastName);
+            jpArray[i].add(  jlFirstName);
+            jpArray[i].add(  jlCourseName);
+            jpArray[i].add(  jlMissedDays);
+            jpArray[i].add(  jlGrade);
+
+
             pane.add(jpArray[i]);
-        }
+
+                 }
+
+
+
 
 
         setVisible(true);
