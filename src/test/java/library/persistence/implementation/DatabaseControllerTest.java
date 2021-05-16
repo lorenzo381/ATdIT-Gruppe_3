5bad1d17cfdfcca090b8826e862ed1032b0972ab
Marco Lorenz <marco.lorenz01@sap.com>
2021-05-16 13:15:55 +0200

    fixed JUnit Tests

--- src/test/java/library/persistence/implementation/DatabaseControllerTest.java
+++ src/test/java/library/persistence/implementation/DatabaseControllerTest.java
@@ -1,9 +1,9 @@
 package library.persistence.implementation;
 
 import library.model.implementation.Student;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DatabaseControllerTest {
 
